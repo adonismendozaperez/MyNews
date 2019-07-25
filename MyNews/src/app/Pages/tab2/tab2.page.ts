ee0b9f13082f8816ff53513bec31eb82c47654ac
Adonis Mendoza <adonis.mendoza@outlook.com>
2019-07-24 21:40:01 -0400

    Add CategoryPage

--- MyNews/src/app/Pages/tab2/tab2.page.ts
+++ MyNews/src/app/Pages/tab2/tab2.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
+import { NewsService } from 'src/app/Services/news.service';
+import { Article } from 'src/app/Models/Interfaces';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page  implements OnInit{
+  @ViewChild(IonSegment) segment : IonSegment;
+ //Variables
+  News:Article[] = [];
+  Category =[
+    "business", "entertainment", "general", "health", "science", "sports", "technology"
+  ]
 
-  constructor() {}
+  constructor(private NewsServices:NewsService) {}
 
+  ngOnInit(){
+    this.segment.value = this.Category[0];
+  }
+
+  getCategory(category) {
+    this.News = [];
+    this.NewsServices.getTopHeadLinesByCategory(category)
+      .subscribe(resp =>{
+        this.News.push(...resp.articles);
+      })
+  }
 }
